da6d4d8acebb1f1cfdfaa759a557acf9004aacc3
Mykola Zekter <nikoliazekter@gmail.com>
2018-05-27 20:48:22 +0300

    Add workout deletion

--- GymPlannerWeb/Controllers/DayController.cs
+++ GymPlannerWeb/Controllers/DayController.cs
@@ -12,7 +12,30 @@ namespace GymPlannerWeb.Controllers
 
         public ActionResult Day(Days day)
         {
+            Session["ID_Day"] = day.ID_Day;
             return View((from d in db.Days where d.ID_Day == day.ID_Day select d).First().Workouts.ToList());
+        }
+
+        public ActionResult DeleteWorkout(Workouts workout)
+        {
+            return View((from w in db.Workouts where w.ID_Workout == workout.ID_Workout select w).First());
+        }
+
+        [HttpPost]
+        [ActionName("DeleteWorkout")]
+        public ActionResult DeleteW(Workouts workout)
+        {
+            var workout2 = db.Workouts.FirstOrDefault(w => w.ID_Workout == workout.ID_Workout);
+            try
+            {
+                db.Workouts.Remove(workout2);
+                db.SaveChanges();
+            }
+            catch (Exception)
+            {
+            }
+            int dayId = (int)Session["ID_Day"];
+            return RedirectToAction("Day", (from d in db.Days where d.ID_Day == dayId select d).First());
         }
     }
 }
