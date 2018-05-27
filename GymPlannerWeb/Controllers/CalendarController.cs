c4a9317a42c8fb1c193b676afa31f781e7b64f5d
Mykola Zekter <nikoliazekter@gmail.com>
2018-05-27 20:56:32 +0300

    Add day deletion

--- GymPlannerWeb/Controllers/CalendarController.cs
+++ GymPlannerWeb/Controllers/CalendarController.cs
@@ -12,7 +12,30 @@ namespace GymPlannerWeb.Controllers
 
         public ActionResult Calendar(Users user)
         {
+            Session["Login"] = user.Login;
             return View((from u in db.Users where u.Login == user.Login select u).First().Days.ToList());
+        }
+
+        public ActionResult DeleteDay(Days day)
+        {
+            return View(day);
+        }
+
+        [HttpPost]
+        [ActionName("DeleteDay")]
+        public ActionResult DeleteD(Days day)
+        {
+            var day2 = db.Days.FirstOrDefault(d => d.ID_Day == day.ID_Day);
+            try
+            {
+                db.Days.Remove(day2);
+                db.SaveChanges();
+            }
+            catch (Exception)
+            {
+            }
+            string login = Session["Login"].ToString();
+            return RedirectToAction("Calendar", (from u in db.Users where u.Login == login select u).First());
         }
     }
 }
